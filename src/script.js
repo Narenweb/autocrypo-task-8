9f1b6c40aacb085639932402d12bfdb3013304a0
“Narayanan <narayanan.sundaram@anywhere.co>
2023-07-12 18:59:29 +0530

    slider added

--- src/script.js
+++ src/script.js
@@ -26,40 +26,42 @@ document.addEventListener('DOMContentLoaded', function () {
     //Slider Functionality
 
     const dots = document.querySelectorAll('.dot');
-const card_container = document.querySelector('.card-container');
-const card = document.querySelector('.card').clientWidth;
-let number = 0;
+    const card_container = document.querySelector('.card-container');
+    const card = document.querySelector('.card').clientWidth;
+    let number = 0;
 
-dots.forEach((dot, indexposition) => {
-    dot.setAttribute("data-number", indexposition);
-    dot.addEventListener('click', (e) => {
-        let DotNumber = e.target.dataset.number;
-        if (DotNumber !== number) {
-            const pixels = -(card + 60) * DotNumber;
-            card_container.style.transform = 'translateX(' + pixels + 'px)';
 
-            const activeCards = document.querySelectorAll('.active');
-            activeCards.forEach((card) => {
-                card.classList.remove("active");
-            });
+    dots.forEach((dot, indexposition) => {
+        dot.setAttribute("data-number", indexposition);
+        dot.addEventListener('click', (event) => {
+            let DotNumber = event.target.dataset.number;
+            console.log(DotNumber);
+            if (DotNumber !== number) {
+                const pixels = -(card+80) * DotNumber;
+                card_container.style.transform = 'translateX(' + pixels + 'px)';
 
-            const activeDots = document.querySelectorAll('.activedot');
-            activeDots.forEach((dot) => {
-                dot.classList.remove("activedot");
-            });
+                const activeCards = document.querySelectorAll('.active');
+                activeCards.forEach((card) => {
+                    card.classList.remove("active");
+                });
 
-            const activeCard = document.querySelector(`[data-id="${DotNumber}"]`);
-            const activedot = document.querySelector(`[data-number="${DotNumber}"]`);
+                const activeDots = document.querySelectorAll('.activedot');
+                activeDots.forEach((dot) => {
+                    dot.classList.remove("activedot");
+                });
 
-            if (activeCard !== null && activedot !== null) {
-                activeCard.classList.add("active");
-                activedot.classList.add("activedot");
-                number = DotNumber;
+                const activeCard = document.querySelector(`[data-id="${DotNumber}"]`);
+                const activedot = document.querySelector(`[data-number="${DotNumber}"]`);
+
+                if (activeCard !== null && activedot !== null) {
+                    activeCard.classList.add("active");
+                    activedot.classList.add("activedot");
+                    number= DotNumber;
+                }
+
             }
-
-        }
+        });
     });
-});
 
 });
 
